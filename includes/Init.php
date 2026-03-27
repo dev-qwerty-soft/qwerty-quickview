e163e11279c6f59192710f8e25c8a6e863967b1d
Danylo Aksynin <daniil.aksinin@gmail.com>
2026-03-27 16:27:40 +0200

    FIX: Updated styles for product card button

--- includes/Init.php
+++ includes/Init.php
@@ -75,7 +75,7 @@ class Init
                                 </button>
                             </div>
 
-                            <button class="qqv-add-to-cart button alt" data-product-id="">
+                            <button class="qqv-add-to-cart" data-product-id="">
                                 Add to cart
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 17 17" fill="none">
